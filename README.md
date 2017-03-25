1a7ac3db755aafdaf183bca01e3ed3ed288a78b2
Giovanni Bruno <giovannididio.bruno@gmail.com>
2017-03-25 02:14:49 +0100

    update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # OHWD17
-Open Hardware Day 2017 Naples
+Open Hardware Day 2017 Naples 25th 2017
+
+
+----------
+## Informazioni importanti ##
+Potete fare riferimento a questa documentazione per le basi della programmazione:  [ESP8266 Arduino](https://github.com/esp8266/Arduino/blob/master/README.md)
+
+Nella cartella [schematic](https://github.com/gbr1/OHWD17/tree/master/schematic) potete trovare un file [Fritzing](http://fritzing.org/home/) che potete modificare per disegnare il vostro schema.
+
+Nella cartella [source](https://github.com/gbr1/OHWD17/tree/master/source) trovate il programma test che è quello precaricato su tutti i kit.
--- source/test/test.ino
+++ source/test/test.ino
@@ -1,13 +1,15 @@
-#include <ESP8266WiFi.h>
+#include <ESP8266WiFi.h>           //header per set istruzioni WiFi
 
+//istruzioni eseguite una sola volta ad avvio
 void setup() {
-  WiFi.mode(WIFI_OFF);
-  pinMode(LED_BUILTIN, OUTPUT);
+  WiFi.mode(WIFI_OFF);		       //chiude la connsessione ad airport e il soft airport
+  pinMode(LED_BUILTIN, OUTPUT);    //il led blu sulla scheda è OUTPUT - non usare in gara
 }
 
+//istruzioni da ripetere all'infinito
 void loop() {
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);  //led spento	
+  delay(1000);					   //aspetto 1s
+  digitalWrite(LED_BUILTIN, HIGH); //led acceso
+  delay(1000);					   //aspetto 1s
 }
